a089616670a1cd4fae02a618eb350928066042d6
Alik <alik220201@gmail.com>
2024-11-14 21:57:46 +0700

    src is ready for use

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+PATH_TO_JSON = BASE_DIR / "Project_OOP" / "data"
+PATH_TO_TEST_JSON = BASE_DIR / "Project_OOP" / "tests"
+
